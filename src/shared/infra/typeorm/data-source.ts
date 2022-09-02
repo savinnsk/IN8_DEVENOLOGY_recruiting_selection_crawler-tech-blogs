ac7f50d84bf3b64e0c7f493c57dd0f642d40858c
Savio Picanço <saviopbrito@outlook.com>
2022-09-01 22:25:52 -0300

    refactor: env files

--- src/shared/infra/typeorm/data-source.ts
+++ src/shared/infra/typeorm/data-source.ts
@@ -4,9 +4,9 @@ import "dotenv/config";
 import { DataSource } from "typeorm"
 
 export const AppDataSource = new DataSource({
-    type: process.env.ORM_TYPE,
+    type: 'postgres',
     host: process.env.ORM_HOST,
-    port: process.env.ORM_PORT,
+    port: 5432,
     username: process.env.ORM_USERNAME,
     password: process.env.ORM_PASSWORD,
     database: process.env.ORM_DATABASE,
