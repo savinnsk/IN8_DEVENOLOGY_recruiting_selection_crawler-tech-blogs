0cd18de121734a3bbecbcfbb6127879d1d12b742
Savio Picanço <saviopbrito@outlook.com>
2022-09-01 21:41:50 -0300

    feat: typeorm initializate

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,4 +1,12 @@
 import {app} from "./app"
 
 
+import { AppDataSource } from "../typeorm/data-source"
+
+AppDataSource.initialize().then(async () => {
+
 app.listen(5000 , ()=> console.log("server is running"))
+
+}).catch(error => console.log(error))
+
+
