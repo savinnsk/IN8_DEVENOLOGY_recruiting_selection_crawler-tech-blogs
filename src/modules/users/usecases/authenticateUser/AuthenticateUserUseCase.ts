79e1b52cc56cd6b8f5db73fd2ac8f62967c46468
Savio Picanço <saviopbrito@outlook.com>
2022-09-04 10:57:00 -0300

    feat: autehenticate usecase

---
+++ src/modules/users/usecases/authenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,53 @@
+
+import "dotenv"
+import { inject, injectable } from "tsyringe";
+import { IUsersRepository } from "../../interfaces/IUsersRepository";
+import {sign} from "jsonwebtoken"
+
+  
+  interface IResponse {
+    user: {
+      name: string;
+    };
+    token: string;
+  }
+
+@injectable()
+class AuthenticateUserUseCase {
+
+    constructor(
+        @inject("UsersRepository")
+        private usersRepository : IUsersRepository
+    ){}
+
+
+    async execute(name : string , password : string){
+
+        const user = await this.usersRepository.findByName(name)
+
+        if(!user){
+            throw new Error("nickname or password invalid")
+        }
+
+        if(password != user.password){
+            throw new Error("nickname or password invalid")
+        }
+
+        const token = sign({}, process.env.USER_SECRET_TOKEN, {
+            subject: user.id,
+            expiresIn: "1d",
+          });
+
+
+          const tokenReturn: IResponse = {
+            token,
+            user: {
+              name: user.name,
+            },
+          };
+      
+          return tokenReturn;
+    }
+}
+
+export {AuthenticateUserUseCase}
