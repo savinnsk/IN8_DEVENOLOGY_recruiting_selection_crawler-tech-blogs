954365bb3eab7df000a32529305d7f5604a909ac
Savio Picanço <saviopbrito@outlook.com>
2022-09-04 11:36:50 -0300

    feat: get autheticate token

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,11 +1,13 @@
 import "reflect-metadata"
 import {Router} from "express"
 import { UsersRoutes } from "./users.routes"
+import { authenticateRoutes } from "./authenticate.routes"
 
 
 const router = Router()
 
 
 router.use("/users" , UsersRoutes)
+router.use("/sessions" , authenticateRoutes)
 
 export {router}
