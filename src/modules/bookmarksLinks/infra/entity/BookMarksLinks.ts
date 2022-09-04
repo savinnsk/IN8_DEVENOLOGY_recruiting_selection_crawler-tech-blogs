cf10fb8923268c5141fdac42838c1431521e1e82
Savio Picanço <saviopbrito@outlook.com>
2022-09-04 12:12:56 -0300

    feat: create bookmarks links entity

---
+++ src/modules/bookmarksLinks/infra/entity/BookMarksLinks.ts
@@ -0,0 +1,34 @@
+import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryColumn } from "typeorm";
+import { v4 as uuidv4 } from "uuid";
+import { User } from "../../../users/infra/typeorm/entity/User";
+
+@Entity("bookmarks-links")
+class BookMarksLinks {
+
+    @PrimaryColumn()
+    id: string
+
+    @Column()
+    title : string
+
+    @Column()
+    user_id : string
+
+    @JoinColumn({ name: "user_id" })
+    @ManyToOne(() => User)
+    user: User;
+
+    @CreateDateColumn()
+    created_at:Date
+
+    constructor() {
+        if(!this.id){
+            this.id = uuidv4()
+        }
+
+}
+
+}
+
+
+export {BookMarksLinks}
