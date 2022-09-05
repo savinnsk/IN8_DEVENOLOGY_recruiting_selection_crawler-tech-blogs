0b7395a247aa8356de8b0b843574e53a52663f2a
Savio Picanço <saviopbrito@outlook.com>
2022-09-05 15:27:26 -0300

    feat: add  interface login

--- src/shared/infra/http/routes/authenticate.routes.ts
+++ src/shared/infra/http/routes/authenticate.routes.ts
@@ -1,10 +1,15 @@
-import { Router } from "express";
+import { Response, Request,Router } from "express";
 import { AuthenticateUserController } from "../../../../modules/users/usecases/authenticateUser/AuthenticateUserController";
+import { upload } from "../../../middlewares/multer";
 
 const authenticateRoutes = Router()
 
 const authenticateUserController = new AuthenticateUserController()
 
-authenticateRoutes.post('/' , authenticateUserController.handle)
+authenticateRoutes.get("/", (request : Request , response : Response ) =>{
+    
+    response.render("login")
+})
+authenticateRoutes.post('/' , upload.fields([{ name: 'name', password : 'password' }]),authenticateUserController.handle)
 
 export {authenticateRoutes}
