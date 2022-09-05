d1fa6ff281ee875d073d05ecbae36975f48be251
Savio Picanço <saviopbrito@outlook.com>
2022-09-05 07:44:31 -0300

    feat: create user page render

--- src/shared/infra/http/routes/users.routes.ts
+++ src/shared/infra/http/routes/users.routes.ts
@@ -1,10 +1,14 @@
 
-import { Router } from "express";
+import { Request, Response,Router } from "express";
 import { CreateUserController } from "../../../../modules/users/usecases/createUser/CreateUserController";
 const UsersRoutes = Router();
 
 
 const createUserController = new CreateUserController()
+
+UsersRoutes.get("/create" , (request : Request , response : Response)=>{
+    response.render("users/create")
+})
 
 UsersRoutes.post("/create" , createUserController.handle )
 
