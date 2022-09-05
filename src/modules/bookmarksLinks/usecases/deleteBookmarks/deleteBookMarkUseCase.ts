db0a815a7f92d87b85df98a086e5d6f0c8feed80
Savio Picanço <saviopbrito@outlook.com>
2022-09-05 02:23:30 -0300

    feat: add delete boomark usecase

--- src/modules/bookmarksLinks/usecases/deleteBookmarks/deleteBookMarkUseCase.ts
+++ src/modules/bookmarksLinks/usecases/deleteBookmarks/deleteBookMarkUseCase.ts
@@ -15,7 +15,18 @@ class DeleteBookMarkUseCase{
 
     async execute(user_id : string , bookmark_id : string) : Promise<void> {
 
+        const bookmark = await this.bookmarksRepository.findById(bookmark_id);
+        const user = await this.usersRepository.findById(user_id);
 
+        if(!user){
+            throw new Error("User doesn't exists")
+        }
+
+        if(!bookmark || user.id != bookmark.user_id){
+            throw new Error("Bookmark invalid")
+        }
+
+        await this.bookmarksRepository.delete(bookmark_id)
     }
 }
 
