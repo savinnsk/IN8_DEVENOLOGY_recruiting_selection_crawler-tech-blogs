1f4771646aa3cfcd6c73390842cc47c5d56c37ba
Savio Picanço <saviopbrito@outlook.com>
2022-09-05 03:10:06 -0300

    feat: add edit bookmark routes

--- src/shared/infra/http/routes/bookmarks.routes.ts
+++ src/shared/infra/http/routes/bookmarks.routes.ts
@@ -1,14 +1,17 @@
 import { Router } from "express";
 import { CreateBookMarkController } from "../../../../modules/bookmarksLinks/usecases/createBookMark/CreateBookMarkController";
 import { DeleteBookmarkController } from "../../../../modules/bookmarksLinks/usecases/deleteBookmarks/deleteBookMarkController";
+import { EditBookMarkController } from "../../../../modules/bookmarksLinks/usecases/editBookMark/EditBookMarkController";
 import { ensureUserAuthenticated } from "../../../middlewares/ensureUserAuthenticate";
 
 const bookmarksRoutes = Router()
 
 const createBookMarkController = new CreateBookMarkController()
 const deleteBookmarkController = new DeleteBookmarkController()
+const editBookMarkController = new EditBookMarkController()
 
 bookmarksRoutes.post("/create" , ensureUserAuthenticated,createBookMarkController.handle)
 bookmarksRoutes.delete("/delete" , ensureUserAuthenticated , deleteBookmarkController.handle)
+bookmarksRoutes.put("/edit" , ensureUserAuthenticated ,editBookMarkController.handle )
 
 export {bookmarksRoutes}
