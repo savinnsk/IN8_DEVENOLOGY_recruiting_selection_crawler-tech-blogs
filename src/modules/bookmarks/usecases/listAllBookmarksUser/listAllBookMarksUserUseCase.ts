007feb7d67a1b88b80764c64e952f44995dca50d
Savio Picanço <saviopbrito@outlook.com>
2022-09-05 04:03:15 -0300

    feat: list all bookmarks usecase

---
+++ src/modules/bookmarks/usecases/listAllBookmarksUser/listAllBookMarksUserUseCase.ts
@@ -0,0 +1,31 @@
+import { inject, injectable } from "tsyringe";
+import { IUsersRepository } from "../../../users/interfaces/IUsersRepository";
+import { Bookmark } from "../../infra/entity/Bookmark";
+import { IBookmarksRepository } from "../../interfaces/IBookmarksLinksRepository";
+
+@injectable()
+class listAllBookMarksUserUseCase {
+
+    constructor(
+        @inject("BookmarkRepository")
+        private bookmarksRepository : IBookmarksRepository,
+        @inject("UsersRepository")
+        private usersRepository : IUsersRepository
+    ){}
+
+
+    async execute(user_id : string) : Promise<Bookmark[]>{
+
+        const user = await this.usersRepository.findById(user_id);
+
+        if(!user){
+            throw new Error("User doesn't exists")
+        }
+
+        const bookmarks = await this.bookmarksRepository.getAll(user_id)
+
+        return bookmarks
+    }
+}
+
+export {listAllBookMarksUserUseCase}
