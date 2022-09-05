1fc42821e4b9a68bb6f517587e7c47ab588ad5f4
Savio Picanço <saviopbrito@outlook.com>
2022-09-05 00:19:18 -0300

    feat: add  bookmarks routes

---
+++ src/shared/infra/http/routes/bookmarks.routes.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import { CreateBookMarkController } from "../../../../modules/bookmarksLinks/usecases/CreateBookMarkController";
+import { ensureUserAuthenticated } from "../../../middlewares/ensureUserAuthenticate";
+
+const bookmarksRoutes = Router()
+
+const createBookMarkController = new CreateBookMarkController()
+
+bookmarksRoutes.post("/create" , ensureUserAuthenticated,createBookMarkController.handle)
+
+export {bookmarksRoutes}
--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -2,6 +2,7 @@ import "reflect-metadata"
 import {Router} from "express"
 import { UsersRoutes } from "./users.routes"
 import { authenticateRoutes } from "./authenticate.routes"
+import { bookmarksRoutes } from "./bookmarks.routes"
 
 
 const router = Router()
@@ -9,5 +10,6 @@ const router = Router()
 
 router.use("/users" , UsersRoutes)
 router.use("/sessions" , authenticateRoutes)
+router.use("/bookmarks" , bookmarksRoutes)
 
 export {router}
