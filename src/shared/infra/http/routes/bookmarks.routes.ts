99544add05db6f9a0f403ab96b18209f76dd83b1
Savio Picanço <saviopbrito@outlook.com>
2022-09-05 02:32:48 -0300

    feat: add  delete a bookmark router

--- src/shared/infra/http/routes/bookmarks.routes.ts
+++ src/shared/infra/http/routes/bookmarks.routes.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
 import { CreateBookMarkController } from "../../../../modules/bookmarksLinks/usecases/createBookMark/CreateBookMarkController";
+import { DeleteBookmarkController } from "../../../../modules/bookmarksLinks/usecases/deleteBookmarks/deleteBookMarkController";
 import { ensureUserAuthenticated } from "../../../middlewares/ensureUserAuthenticate";
 
 const bookmarksRoutes = Router()
 
 const createBookMarkController = new CreateBookMarkController()
+const deleteBookmarkController = new DeleteBookmarkController()
 
 bookmarksRoutes.post("/create" , ensureUserAuthenticated,createBookMarkController.handle)
+bookmarksRoutes.delete("/delete" , ensureUserAuthenticated , deleteBookmarkController.handle)
 
 export {bookmarksRoutes}
