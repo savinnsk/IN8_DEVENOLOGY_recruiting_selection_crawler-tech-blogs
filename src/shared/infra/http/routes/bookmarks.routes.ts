fa766d429a4082be1bd593fa62d7ef4dea92beec
Savio Picanço <saviopbrito@outlook.com>
2022-09-05 04:11:59 -0300

    feat: list all bookmarks routes

--- src/shared/infra/http/routes/bookmarks.routes.ts
+++ src/shared/infra/http/routes/bookmarks.routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { CreateBookMarkController } from "../../../../modules/bookmarks/usecases/createBookMark/CreateBookMarkController";
 import { DeleteBookmarkController } from "../../../../modules/bookmarks/usecases/deleteBookmarks/deleteBookMarkController";
 import { EditBookMarkController } from "../../../../modules/bookmarks/usecases/editBookMark/EditBookMarkController";
+import { ListAllBookmarksUserController } from "../../../../modules/bookmarks/usecases/listAllBookmarksUser/listAllBookmarksUserController";
 import { ensureUserAuthenticated } from "../../../middlewares/ensureUserAuthenticate";
 
 const bookmarksRoutes = Router()
@@ -9,9 +10,11 @@ const bookmarksRoutes = Router()
 const createBookMarkController = new CreateBookMarkController()
 const deleteBookmarkController = new DeleteBookmarkController()
 const editBookMarkController = new EditBookMarkController()
+const listAllBookmarksUserController = new ListAllBookmarksUserController()
 
 bookmarksRoutes.post("/create" , ensureUserAuthenticated,createBookMarkController.handle)
 bookmarksRoutes.delete("/delete/:id" , ensureUserAuthenticated , deleteBookmarkController.handle)
 bookmarksRoutes.put("/edit/:id" , ensureUserAuthenticated ,editBookMarkController.handle )
+bookmarksRoutes.get("/list" , ensureUserAuthenticated , listAllBookmarksUserController.handle)
 
 export {bookmarksRoutes}
