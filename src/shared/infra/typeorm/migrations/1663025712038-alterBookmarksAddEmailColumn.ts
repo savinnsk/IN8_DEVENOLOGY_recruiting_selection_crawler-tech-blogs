85d6682ccd95c6b1df9f6eda254604a8acb08b19
Savio Picanço <saviopbrito@outlook.com>
2022-09-12 20:39:17 -0300

    feat: add column email database users

---
+++ src/shared/infra/typeorm/migrations/1663025712038-alterBookmarksAddEmailColumn.ts
@@ -0,0 +1,20 @@
+import { Column, MigrationInterface, QueryRunner, TableColumn } from "typeorm"
+
+export class alterBookmarksAddEmailColumn1663025712038 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+
+        await queryRunner.addColumn("bookmarks-links" , new TableColumn(
+            {
+                name:"email",
+                type:"varchar"
+            }
+            ))
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+
+        await queryRunner.dropColumn("email")
+    }
+
+}
