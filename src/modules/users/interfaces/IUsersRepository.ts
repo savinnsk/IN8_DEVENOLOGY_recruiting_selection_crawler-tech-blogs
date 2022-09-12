a980664867a6d513101efae4438f44bf583d3bbc
Savio Picanço <saviopbrito@outlook.com>
2022-09-12 20:41:25 -0300

    refactor: users repository interface

--- src/modules/users/interfaces/IUsersRepository.ts
+++ src/modules/users/interfaces/IUsersRepository.ts
@@ -2,7 +2,8 @@ import { User } from "../infra/typeorm/entity/User";
 //import { UserDTO } from "./UserDTOr";
 
 export interface IUsersRepository {
-    create(name : string , password : string): Promise<User>
+    create(name : string , password : string , email: string): Promise<User>
     findByName(name : string) : Promise<User>
     findById(id : string) : Promise <User>
+    findByEmail(email : string) : Promise<User>
 }
