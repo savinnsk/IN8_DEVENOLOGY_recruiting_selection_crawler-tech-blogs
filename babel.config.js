32094eb55d5699ede4a85590414d89b64e012367
Savio Picanço <saviopbrito@outlook.com>
2022-09-19 20:22:19 -0300

    config: refactor babel packages.json

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,7 @@
 module.exports = {
    presets : 
    [
-    ["@babel/preset-env" , {target : { node : "current"}}],
+    ["@babel/preset-env" , {targets : { node : "current"}}],
     "@babel/preset-typescript"
    ],
    plugins : [
