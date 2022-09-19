24a8233af79d2e5dff4bbacf531a63190d987874
Savio Picanço <saviopbrito@outlook.com>
2022-09-19 19:48:38 -0300

    chore: add babel and plugins

---
+++ babel.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+   presets : 
+   [
+    ["@babel/preset-env" , {target : { node : "current"}}],
+    "@babel/preset-typescript"
+   ],
+   plugins : [
+      [
+         "module-resolver",
+         {
+            alias : {}
+         }
+      ]
+   ] 
+}
