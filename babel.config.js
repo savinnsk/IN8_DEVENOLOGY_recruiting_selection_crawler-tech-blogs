0e9378d1d26e3cb8bad08e5a9f32be31aa3d2cea
Savio Picanço <saviopbrito@outlook.com>
2022-09-19 19:52:42 -0300

    config: add babel config

--- babel.config.js
+++ babel.config.js
@@ -10,6 +10,9 @@ module.exports = {
          {
             alias : {}
          }
-      ]
+      ],
+      "babel-plugin-transform-typescript-metadata",
+      ["@babel/plugin-proposal-decorators" , {legacy : true}],
+      ["@babel/plugin-proposal-class-properties" , {loose : true}],
    ] 
 }
