71de7df80b551a5e3e353d2dda8b52efe35a1f0c
savinnsk <saviodocuments@gmail.com>
2022-09-21 15:06:50 -0300

    Update index.ts

--- src/shared/services/puppeteer/index.ts
+++ src/shared/services/puppeteer/index.ts
@@ -7,7 +7,7 @@ async function crawlerWebBlog(devGoLink) {
 
  
   try{
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
   
   const page = await browser.newPage();
   await page.goto(`${devGoLink}`);
